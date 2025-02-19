d77c67c72c633f0b611c82a254f9c6df1cf3373d
Amrita Sawhney <amritak.saw@gmail.com>
2025-02-18 20:49:25 -0500

    fixing background img imports

--- src/Components/spotifyAPI/page-array.jsx
+++ src/Components/spotifyAPI/page-array.jsx
@@ -1,32 +1,38 @@
+import upsetImg from "/src/assets/upset-background.jpg"
+import tiredImg from "/src/assets/tired-background.jpg"
+import excitedImg from "/src/assets/excited-background.jpg"
+import proudImg from "/src/assets/proud-background.jpg"
+import stressedImg from "/src/assets/stressed-background.jpg"
+
 export const pageDataArray = [
     {
       id: "4yN7w2k29EjRTSuSsk0H83",
       name: "stressed",
-      image: "/src/assets/upset-background.jpeg",
+      image: upsetImg,
       consider: "stretching"
     },
     {
       id: "7MQ9d9fffu2XIFdLEj2qno",
       name: "tired",
-      image: "/src/assets/tired-background.jpeg",
+      image: tiredImg,
       consider: "going to bed"
     },
     {
       id: "621IjaU1LGS1OPgLtPvMeT",
       name: "upset",
-      image: "/src/assets/excited-background.jpeg",
+      image: excitedImg,
       consider: "feeling some grass"
     },
     {
       id: "7INcD4lmarWTQiDVodjVt4",
       name: "proud",
-      image: "/src/assets/stressed-background.jpeg",
+      image:  stressedImg,
       consider: "telling your mom you love her"
     },
     {
       id: "64oKoqB5rfgVSzZ0WIiqXc",
       name: "excited",
-      image: "/src/assets/proud-background.jpeg",
+      image: proudImg,
       consider: "running through the streets"
     }
   ]
